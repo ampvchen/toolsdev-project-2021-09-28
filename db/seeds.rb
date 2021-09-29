ff98924db7df9ed2ebb96cb7a82d99c2a860325d
Victor <victor@chen.plus>
2021-09-29 16:54:36 -0500

    add location model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create(name: 'Austin', lat: 30.404251, long: -97.849442)
