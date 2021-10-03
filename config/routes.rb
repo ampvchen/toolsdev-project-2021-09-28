6d8bb058f9b524d03946bf2baed15507ace00ae7
Victor <victor@chen.plus>
2021-10-03 13:45:34 -0500

    add sidekiq routes for development

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
@@ -7,4 +9,9 @@ Rails.application.routes.draw do
     get 'temperatures/forecast', to: 'temperatures#forecast'
     get 'temperatures/high_low', to: 'temperatures#high_low'
   end
+
+  # Limit access to Development
+  # TODO: Setup Devise to make the WebUI only available to :admin
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
+
 end
