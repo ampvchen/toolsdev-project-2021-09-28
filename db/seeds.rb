da5644994ca18a7f431da4bba6647821c14c8f9d
Victor <victor@chen.plus>
2021-10-03 20:12:12 -0500

    update locations seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,64 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Location.create(location_id: 'ATX1', nickname: 'Austin HQ', city: 'Austin', state: 'TX', lat: 30.404251, long: -97.849442)
+Location.create(
+  location_id: 'AUS1',
+  nickname: 'Austin HQ',
+  city: 'Austin',
+  state: 'TX',
+  country: 'United States',
+  lat: 30.404251,
+  long: -97.849442
+)
+
+Location.create(
+  location_id: 'AUS2',
+  nickname: 'Four Points',
+  city: 'Austin',
+  state: 'TX',
+  country: 'United States',
+  lat: 30.268525,
+  long: -97.740958
+)
+
+Location.create(
+  location_id: 'SFO1',
+  city: 'San Francisco',
+  state: 'CA',
+  country: 'United States',
+  lat: 37.787930,
+  long: -122.402592
+)
+
+Location.create(
+  location_id: 'SYD1',
+  city: 'Sydney',
+  country: 'Australia',
+  lat: -33.868279,
+  long: 151.208678
+)
+
+Location.create(
+  location_id: 'DUB1',
+  city: 'Dublin',
+  country: 'Ireland',
+  lat: 53.339484,
+  long: -6.252009
+)
+
+Location.create(
+  location_id: 'IEV1',
+  city: 'Kyiv',
+  country: 'Ukraine',
+  lat: 50.467021,
+  long: 30.447241
+)
+
+Location.create(
+  location_id: 'LCY1',
+  city: 'London',
+  country: 'United Kingdom',
+  lat: 51.507737,
+  long: -0.073803
+)
+
