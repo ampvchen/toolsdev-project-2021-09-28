dbbb5eb67512ad5141720d6315ddc62179551fd9
Victor <victor@chen.plus>
2021-10-03 22:53:46 -0500

    add temperature model validation test

--- test/models/temperature_test.rb
+++ test/models/temperature_test.rb
@@ -4,4 +4,21 @@ class TemperatureTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  test 'should not save temperature if one exists for same location and time' do
+    datetime = DateTime.now
+
+    Temperature.create(
+      location_id: 1,
+      datetime: datetime
+    )
+
+    temperature = Temperature.create(
+      location_id: 1,
+      datetime: datetime
+    )
+
+    assert_not temperature.save
+
+  end
 end
